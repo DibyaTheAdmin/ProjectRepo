588e64cc70b2d1c0a6c94d1c0c0f4d71aa4d3722
DELL <DELL@ADMINRG-1EJELKO>
2020-09-11 13:49:11 +0530

    3rd changes by Dibya about home,index,mycontroller added.....

---
+++ Master_Project/src/main/java/com/dr/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.dr.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/api/hi")
+	public String returnHome() {
+		return "home";
+	}
+}
